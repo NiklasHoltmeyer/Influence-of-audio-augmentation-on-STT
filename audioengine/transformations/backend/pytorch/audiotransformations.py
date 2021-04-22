59238bf1ee6d2d5950998388b2dae7401bae5144
Niklas Holtmeyer <>
2021-04-22 09:48:31 +0200

    preprocess

--- audioengine/transformations/backend/pytorch/audiotransformations.py
+++ audioengine/transformations/backend/pytorch/audiotransformations.py
@@ -1,12 +1,12 @@
 import torchaudio
 
+
 class LoadAudio(object):
     def __init__(self, input_sr=None, output_sr=None, to_numpy=True):
         self.sr = output_sr
         self.sr_default = input_sr
         self.resampler = torchaudio.transforms.Resample(input_sr, output_sr) if input_sr and output_sr else None
         self.to_numpy = to_numpy
-
 
     def __call__(self, data):
         waveform, _ = torchaudio.load(data['path'])
@@ -20,19 +20,20 @@ class LoadAudio(object):
         data["speech"] = waveform.squeeze(0).numpy() if self.to_numpy else waveform.squeeze(0)
         return data
 
+
 class PreprocessTransformer:
-    def __init__(self, processor, sampling_rate):
+    def __init__(self, processor, sampling_rate, padding=True):
         self.processor = processor
         self.sampling_rate = sampling_rate
+        self.padding = padding
 
     def __call__(self, batch):
-        batch["input_values"] = self.processor(batch["speech"], return_tensors="pt", sampling_rate=16_000, padding=True).input_values
-        #inputs = self.processor(speeches, sampling_rate=sampling_rate, return_tensors="pt", padding=padding)
+        batch["input_values"] = self.processor(batch["speech"]).input_values
+        #return_tensors="pt", sampling_rate=self.sampling_rate, padding=self.padding
         with self.processor.as_target_processor():
-            batch["labels"] = self.processor(batch["sentence"],return_tensors="pt", padding=True).input_ids
+            batch["labels"] = self.processor(batch["sentence"]).input_ids
+            #batch["labels"] = self.processor(batch["sentence"], return_tensors="pt", padding=self.padding).input_ids
         return batch
-
-
 
 ##class Spectrogram:
 ##    def __init__(self, n_fft: int = 400,
@@ -47,7 +48,7 @@ class PreprocessTransformer:
 ##                 pad_mode: str = "reflect",
 ##                 onesided: bool = True):
 ##        self.transform = torchaudio.transforms.Spectrogram(n_fft, win_length, hop_length, pad, window_fn, power,
-                                                           ##normalized, wkwargs, center, pad_mode, onesided)
+##normalized, wkwargs, center, pad_mode, onesided)
 
 ##    def __call__(self, data):
 ##        data["spectrogram"] = self.transform.forward(data["speech"])
