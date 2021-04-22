859135d13e93aa5d5683c2badb39431688358ddb
Niklas Holtmeyer <>
2021-04-22 14:00:24 +0200

    hugging audio

--- audioengine/transformations/backend/pytorch/huggingfacetransformations.py
+++ audioengine/transformations/backend/pytorch/huggingfacetransformations.py
@@ -1,13 +1,31 @@
-def process_inputs(processor):
-    def __call__(batch):
-        # check that all files have the correct sampling rate
-        assert (
-                len(set(batch["sampling_rate"])) == 1
-        ), f"Make sure all inputs have the same sampling rate of {processor.feature_extractor.sampling_rate}."
+import torchaudio
 
-        batch["input_values"] = processor(batch["speech"], sampling_rate=batch["sampling_rate"][0]).input_values
 
-        with processor.as_target_processor():
-            batch["labels"] = processor(batch["target_text"]).input_ids
+class LoadandResampleAudio:
+    def __init__(self, processor, **kwargs):
+        self.processor = processor
+        self.speech_key = kwargs.get("speech_key", "speech")
+        self.sentence_key = kwargs.get("sentence_key", "sentence")
+
+        self.sample_rate_in = kwargs.get("sample_rate_in", None)
+        self.sample_rate_out = kwargs.get("sample_rate_in", None)
+
+        self.resampler = torchaudio.transforms.Resample(self.sample_rate_in,
+                                                        self.sample_rate_out) if self.sample_rate_in and self.sample_rate_out else None
+
+    def __call__(self, batch):
+        waveform, sampling_rate = torchaudio.load(batch[self.speech_key])
+        #batch["speech"] = speech_array[0].numpy()
+        #batch["sampling_rate"] = sampling_rate
+        #batch["target_text"] = batch["text"]
+
+        if self.resampler:
+            waveform = self.resampler(waveform)
+            batch["speech"] = waveform[0].numpy()
+            batch["sampling_rate"] = self.sample_rate_out
+        else:
+            batch["speech"] = waveform[0].numpy()
+            batch["sampling_rate"] = sampling_rate
+
         return batch
-    return __call__
+
