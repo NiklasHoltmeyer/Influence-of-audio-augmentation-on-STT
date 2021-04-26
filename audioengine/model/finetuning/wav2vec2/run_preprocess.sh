f63402429745e2a61a16da27ef6cec61fb24ed48
Niklas Holtmeyer <>
2021-04-26 09:11:01 +0200

    tune-parm

--- audioengine/model/finetuning/wav2vec2/run_preprocess.sh
+++ audioengine/model/finetuning/wav2vec2/run_preprocess.sh
@@ -1,17 +1,18 @@
-python3 preprocess_dataset.py --model_name_or_path="maxidl/wav2vec2-large-xlsr-german" \
+python3 preprocess_dataset.py \
+--model_name_or_path="maxidl/wav2vec2-large-xlsr-german" \
 --dataset_config_name="de" \
---output_dir=/share/train/w2v/ \
+--output_dir="/share/w2v/wav2vec2-large-xlsr-german-sm" \
 --preprocessing_num_workers="8" \
 --overwrite_output_dir \
 --num_train_epochs="1" \
---per_device_train_batch_size="32" \
---per_device_eval_batch_size="32" \
---learning_rate="1e-4" \
+--per_device_train_batch_size="1" \
+--per_device_eval_batch_size="1" \
+--learning_rate="1e-5" \
 --warmup_steps="500" \
 --evaluation_strategy="steps" \
---save_steps="5000" \
---eval_steps="5000" \
---logging_steps="1000" \
+--save_steps="400" \
+--eval_steps="400" \
+--logging_steps="400" \
 --save_total_limit="3" \
 --freeze_feature_extractor \
 --activation_dropout="0.055" \
@@ -20,10 +21,13 @@ python3 preprocess_dataset.py --model_name_or_path="maxidl/wav2vec2-large-xlsr-g
 --layerdrop="0.04" \
 --mask_time_prob="0.08" \
 --gradient_checkpointing="1" \
+--eval_accumulation_steps="2" \
 --fp16 \
 --do_train \
 --do_eval \
 --dataloader_num_workers="8" \
---group_by_length
+--group_by_length \
+--preprocess_dataset_path="/share/datasets/vf_de/" \
+--dataset_path="/share/datasets/vf_de_processed/"
 #.max_train_samples
 #max_val_samples
