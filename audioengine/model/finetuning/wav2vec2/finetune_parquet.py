87ea5ffb013d0ed4462a6824521512c28e7c92be
Niklas Holtmeyer <>
2021-04-27 13:41:36 +0200

    training

--- audioengine/model/finetuning/wav2vec2/finetune_parquet.py
+++ audioengine/model/finetuning/wav2vec2/finetune_parquet.py
@@ -60,7 +60,7 @@ def main():
     logger.warning(f"Load Model {model_args.model_name_or_path}")
     logger.warning(f"* Cache_Dir {model_args.cache_dir}")
 
-    if training_args.do_train: #load_last fine_tuned
+    if False: #not training_args.do_train: #load_last fine_tuned
         logger.debug(f"Loading Fine-Tuned {training_args.output_dir}")
         processor = Wav2Vec2Processor.from_pretrained(training_args.output_dir) #eval last fined_tuned_model
         model = Wav2Vec2ForCTC.from_pretrained( #eval last fined_tuned_model
